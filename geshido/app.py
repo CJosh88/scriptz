310849c9b69d15a154a3dd1d35a2ddf1572a59e4
CJosh88 <60396943+CJosh88@users.noreply.github.com>
2024-07-21 18:54:27 +0200

    Update app.py

--- geshido/app.py
+++ geshido/app.py
@@ -119,6 +119,7 @@ def main():
     #set_background("geshido/bk.jpg")
     
     st.header("💬 Using AI Agents to generate/simulate product roadmap and backlog artefacts")
+    st.empty()
     st.subheader('An IQbusiness AI Lab demo', divider='rainbow')
 
     if "messages" not in st.session_state:
